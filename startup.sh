a8dd932a2e7210cd39dd1df448881122e3b18a16
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2023-10-17 18:16:11 +0100

    Add startup script point to live search api

---
+++ startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+bundle check || bundle install
+
+if [[ $1 == "--live" ]] ; then
+  GOVUK_APP_DOMAIN=www.gov.uk \
+  GOVUK_WEBSITE_ROOT=https://www.gov.uk \
+  GOVUK_PROXY_STATIC_ENABLED=true \
+  PLEK_SERVICE_SEARCH_API_URI=${PLEK_SERVICE_SEARCH_API_URI-https://www.gov.uk/api} \
+  PLEK_SERVICE_CONTENT_STORE_URI=${PLEK_SERVICE_CONTENT_STORE_URI-https://www.gov.uk/api} \
+  PLEK_SERVICE_STATIC_URI=${PLEK_SERVICE_STATIC_URI-https://assets.publishing.service.gov.uk} \
+  bundle exec rails s -p 3000
+else
+  echo "ERROR: other startup modes are not supported"
+  echo ""
+  echo "https://docs.publishing.service.gov.uk/manual/local-frontend-development.html"
+  exit 1
+fi
