1cd2a35b99a22fee0f421b50c37b3cf705d18910
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2023-10-24 15:13:43 +0100

    Add alias for organisation sitemaps route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get "/healthcheck/live", to: proc { [200, {}, %w[OK]] }
   get "/healthcheck/ready", to: GovukHealthcheck.rack_response
 
-  get "/sitemaps/:organisation_slug", to: "organisation#show", defaults: { format: "xml" }
+  get "/sitemaps/:organisation_slug", to: "organisation#show", defaults: { format: "xml" }, as: "organisation_sitemap"
 end
