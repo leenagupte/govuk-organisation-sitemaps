1ea6d0168cecf06d19c21d0d6fa71f775b454247
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2023-10-24 15:17:07 +0100

    Display sitemap index
    Lists links to sitemaps for all organisations that publish on GOV.UK
    No styling has been applied yet.
    
    stylesheet_link_tag in the application html has been removed because it was throwing an error.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "/healthcheck/ready", to: GovukHealthcheck.rack_response
 
   get "/sitemaps/:organisation_slug", to: "organisation#show", defaults: { format: "xml" }, as: "organisation_sitemap"
+  get "sitemaps", to: "organisation#index"
 end
