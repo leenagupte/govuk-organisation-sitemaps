5ee977475593eebecda6eb448655ad9a6add927b
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2023-10-25 14:14:52 +0100

    Simply how organisations are retrieved
    Use the organisations api

--- app/models/organisations.rb
+++ app/models/organisations.rb
@@ -1,64 +1,21 @@
-require "gds_api/content_store"
+require "gds_api/organisations"
 require "plek"
 
 class Organisations
 
   def all
-    all_organisations.map do |organisation|
+    organisations.map do |organisation|
       {
-        title: organisation[:title],
-        slug: organisation[:slug],
-        acronym: organisation[:acronym],
+        title: organisation["title"],
+        slug: organisation["details"]["slug"],
+        acronym: organisation["details"]["abbreviation"],
       }
     end
   end
 
 private
 
-  def all_organisations
-    organisations = []
-    
-    organisation_content_item[:details][:ordered_agencies_and_other_public_bodies].each do |organisation|
-      organisations << organisation
-    end
-
-    organisation_content_item[:details][:ordered_devolved_administrations].each do |organisation|
-      organisations << organisation
-    end
-
-    organisation_content_item[:details][:ordered_executive_offices].each do |organisation|
-      organisations << organisation
-    end
-
-    organisation_content_item[:details][:ordered_high_profile_groups].each do |organisation|
-      organisations << organisation
-    end
-
-    organisation_content_item[:details][:ordered_ministerial_departments].each do |organisation|
-      organisations << organisation
-    end
-
-    organisation_content_item[:details][:ordered_non_ministerial_departments].each do |organisation|
-      organisations << organisation
-    end
-
-    organisation_content_item[:details][:ordered_public_corporations].each do |organisation|
-      organisations << organisation
-    end
-
-    organisations
-  end
-
-  def organisation_content_item
-    response = GdsApi::ContentStore.new(content_store_url).content_item(base_path)
-    response.to_h.deep_symbolize_keys
-  end
-
-  def base_path
-    "/government/organisations"
-  end
-
-  def content_store_url
-    Plek.find("content-store")
+  def organisations
+    GdsApi::Organisations.new(Plek.website_root).organisations.with_subsequent_pages
   end
 end
